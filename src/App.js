81029df75285d6a4bc22ed63b451801068956389
Jen Bird <jenniredfield@hotmail.com>
2018-02-20 17:27:04 +0000

    Cleaned up

--- src/App.js
+++ src/App.js
@@ -12,65 +12,58 @@ class App extends Component {
 
   }
 
-
   handleInput = (event) => {
 
-
     this.setState({
-       userInput: event.target.value,
+      userInput: event.target.value,
     })
 
   }
-
 
   fetchRepo = (repo) => {
 
-    
-    if(this.state.userInput === "") {
+    if (this.state.userInput === "") {
       return;
     }
 
-    repo = this.state.userInput; 
+    repo = this.state.userInput;
 
-    
     fetch(`https://api.github.com/search/repositories?q=${repo}`)
-    .then(res => {
-      return res.json();
-    }).then(res => {
-      // console.log(res)
-     if(res.total_count === 0) {
-       this.setState({
-         repoNotFound: true,
-       })
-     }
+      .then(res => {
+        return res.json();
+      }).then(res => {
+        // console.log(res)
+        if (res.total_count === 0) {
+          this.setState({
+            repoNotFound: true,
+          })
+        }
 
-     let sortedResult = res.items.sort((a,b)=> {
+        let sortedResult = res.items.sort((a, b) => {
           return b.forks_count - a.forks_count;
-     })
+        })
 
-       this.setState({
-         repos : sortedResult,
-         repoNotFound: false,
-       })
-    })
+        this.setState({
+          repos: sortedResult,
+          repoNotFound: false,
+        })
+      })
 
   }
 
-
   render() {
-
 
     return (
       <div className="App">
         <div className="header-wrapper">
-            <h1>Github Repo Finder</h1>
-            <small>Displays first 30 repos sorted by the number of forks</small>
-    
-              <input onChange={this.handleInput} onKeyPress={(event) => { if(event.key === 'Enter') this.fetchRepo() }} placeholder="i.e tetris, hangman etc"></input>
-              <button onClick={this.fetchRepo}>Find Repo!</button>
-      
+          <h1>Github Repo Finder</h1>
+          <small>Displays first 30 repos sorted by the number of forks</small>
+
+          <input onChange={this.handleInput} onKeyPress={(event) => { if (event.key === 'Enter') this.fetchRepo() }} placeholder="i.e tetris, hangman etc"></input>
+          <button onClick={this.fetchRepo}>Find Repo!</button>
+
         </div>
-        <Repos repos={this.state.repos} repoNotFound={this.state.repoNotFound}/>
+        <Repos repos={this.state.repos} repoNotFound={this.state.repoNotFound} />
 
       </div>
     );
