415cf127a54a1bdb2aa7b2dc66e1d6a92072833e
Jen Bird <jenniredfield@hotmail.com>
2018-02-20 15:40:33 +0000

    Change style

--- src/Repos.js
+++ src/Repos.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 const Repos = ({ repos }) => {
 
-    if(repos.length=== 0) return "";
+    if (repos.length === 0) return "";
 
     return (
 
@@ -13,17 +13,22 @@ const Repos = ({ repos }) => {
                 return (
                     <div className="repo-div">
                         <div className="grid-1">
-                            <img src={repo.owner.avatar_url} alt="user-logo" style={{ width: "60px"}}/>
+                            <img src={repo.owner.avatar_url} alt="user-logo" style={{ width: "60px" }} />
                         </div>
                         <div className="grid-2">
                             <ul>
-                                <li>{repo.description}</li>
-                                <li>{repo.name}</li>
-                                <li>{repo.owner.login}</li>
+                                <li>Name: {repo.name}</li>
+                                <li>Description: {repo.description}</li>
+                                <li>Created by: {repo.owner.login}</li>
+                                <li>Language: {repo.language}</li>
                             </ul>
                         </div>
                         <div className="grid-3">
-
+                            <ul>
+                                <li>Number of forks: {repo.forks_count}</li>
+                                <li>Stargazers: {repo.stargazers_count}</li>
+                                <li>Number of issues: {repo.open_issues_count}</li>
+                            </ul>
                         </div>
                     </div>
                 )
