858d8935d4b8765b40d1f9f25249abc0d2b4d441
Jen Bird <jenniredfield@hotmail.com>
2018-02-21 12:27:40 +0000

    Added linting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Repos from './Repos.js'
+import Repos from './Repos.js';
 
 class App extends Component {
 
   state = {
 
-    userInput: "",
+    userInput: '',
     repos: [],
     repoNotFound: false,
     loading: false,
@@ -16,19 +16,19 @@ class App extends Component {
 
     this.setState({
       userInput: event.target.value,
-    })
+    });
 
   }
 
   fetchRepo = () => {
 
-    if (this.state.userInput === "") {
+    if (this.state.userInput === '') {
       return;
     }
 
     this.setState({
       loading: true,
-    })
+    });
 
 
     fetch(`https://api.github.com/search/repositories?q=${this.state.userInput}`)
@@ -40,19 +40,19 @@ class App extends Component {
           this.setState({
             repoNotFound: true,
             loading: false,
-          })
+          });
         }
 
         let sortedResult = res.items.sort((a, b) => {
           return b.forks_count - a.forks_count;
-        })
+        });
 
         this.setState({
           repos: sortedResult,
           repoNotFound: false,
           loading: false,
-        })
-      })
+        });
+      });
 
   }
 
@@ -64,7 +64,7 @@ class App extends Component {
           <h1>Github Repo Finder</h1>
           <small>Displays first 30 repos sorted by the number of forks</small>
 
-          <input onChange={this.handleInput} onKeyPress={(event) => { if (event.key === 'Enter') this.fetchRepo() }} placeholder="i.e tetris, hangman etc"></input>
+          <input onChange={this.handleInput} onKeyPress={(event) => { if (event.key === 'Enter') this.fetchRepo(); }} placeholder="i.e tetris, hangman etc"></input>
           <button onClick={this.fetchRepo}>Find Repo!</button>
 
         </div>
--- src/Repos.js
+++ src/Repos.js
@@ -3,44 +3,44 @@ import React from 'react';
 
 const Repos = ({ repos, repoNotFound, loading }) => {
 
-    if(loading) return ( <div className="loading"><div className="loader"></div></div>)
-    if(repoNotFound) return ( <div className="repos-not-found"><h2>Sorry, no repos were found!</h2></div>);
-    if(repos.length === 0) return "";
+  if(loading) return ( <div className="loading"><div className="loader"></div></div>);
+  if(repoNotFound) return ( <div className="repos-not-found"><h2>Sorry, no repos were found!</h2></div>);
+  if(repos.length === 0) return '';
    
-    return (
+  return (
 
-        <div className="repos-wrapper"> {
+    <div className="repos-wrapper"> {
 
-            repos.map((repo, i) => {
-                return (
-                    <div className="repo-div" key={i}>
-                        <div className="grid-1">
-                            <a href={repo.owner.html_url}><img src={repo.owner.avatar_url} alt="user-logo" style={{ width: "60px" }} /></a>
-                        </div>
-                        <div className="grid-2">
-                            <ul>
-                                <li>Name: {repo.name}</li>
-                                <li>Description: {repo.description}</li>
-                                <li>Created by: {repo.owner.login}</li>
-                                <li>Language: {repo.language}</li>
-                            </ul>
-                        </div>
-                        <div className="grid-3">
-                            <ul>
-                                <li>Number of forks: {repo.forks_count}</li>
-                                <li>Stargazers: {repo.stargazers_count}</li>
-                                <li>Number of issues: {repo.open_issues_count}</li>
-                                <li><a href={`https://github.com/${repo.full_name}`}><button>{repo.full_name}</button></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                )
+      repos.map((repo, i) => {
+        return (
+          <div className="repo-div" key={i}>
+            <div className="grid-1">
+              <a href={repo.owner.html_url}><img src={repo.owner.avatar_url} alt="user-logo" style={{ width: '60px' }} /></a>
+            </div>
+            <div className="grid-2">
+              <ul>
+                <li>Name: {repo.name}</li>
+                <li>Description: {repo.description}</li>
+                <li>Created by: {repo.owner.login}</li>
+                <li>Language: {repo.language}</li>
+              </ul>
+            </div>
+            <div className="grid-3">
+              <ul>
+                <li>Number of forks: {repo.forks_count}</li>
+                <li>Stargazers: {repo.stargazers_count}</li>
+                <li>Number of issues: {repo.open_issues_count}</li>
+                <li><a href={`https://github.com/${repo.full_name}`}><button>{repo.full_name}</button></a></li>
+              </ul>
+            </div>
+          </div>
+        );
 
-            })}
-        </div>
-    )
+      })}
+    </div>
+  );
   
-}
+};
 
 
 
